2e71318b12300e1446d5e71ea5211de0cc959997
eggman1333 <zackmanthony@gmail.com>
2025-08-04 13:44:32 -0400

    Initialize Asteroids project with essential files and configurations

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
