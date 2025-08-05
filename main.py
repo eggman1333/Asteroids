0d83671e67cdc7f0bb00ec80d94ea4605c41c02b
eggman1333 <zackmanthony@gmail.com>
2025-08-05 11:09:58 -0400

    grouped updateable_sprites and drawable_sprites

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updateable_sprites = pygame.sprite.Group()
+    drawable_sprites = pygame.sprite.Group()
+    
+    Player.containers = (updateable_sprites, drawable_sprites)
+
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Instantiate Player at screen center
+    
     running = True
     dt = 0
 
@@ -19,9 +26,13 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
+        updateable_sprites.update(dt)
+
         screen.fill("black")
-        player.draw(screen) # Draw the player
-        player.update(dt)
+        
+        for obj in drawable_sprites:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000.0
